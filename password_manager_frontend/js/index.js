5f03153ddd68826c7a7c369289e2245a4022b9d3
toddnguyen47 <todd.nguyen47@gmail.com>
2018-08-09 20:54:24 -0700

    Rough front end done

---
+++ password_manager_frontend/js/index.js
@@ -0,0 +1,7 @@
+// Only load the script when the website is ready
+$(document).ready(function(){
+    // Submit button logic
+    $("#submitButton").click(function() {
+        alert("You clicked this button!");
+    });
+});
